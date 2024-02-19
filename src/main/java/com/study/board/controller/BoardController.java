2f694d198537e012363fee8f8ddc940a5115c90c
Jimyeong Kim <kjm021221@icloud.com>
2024-02-19 14:33:18 +0900

    feat: Service Repository Entity 패키지 추가

--- src/main/java/com/study/board/controller/BoardController.java
+++ src/main/java/com/study/board/controller/BoardController.java
@@ -1,14 +1,29 @@
 package com.study.board.controller;
 
+import com.study.board.entity.Board;
+import com.study.board.service.BoardService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class BoardController {
-    @GetMapping("/")
+
+    @Autowired
+    private BoardService boardService;
+
+    @GetMapping("/board/write")
     public String boardWriteForm(){
 
         return "boardwrite";
+    }
+
+    @PostMapping("/board/writepro")
+    public String boardWritePro(Board board){
+
+        boardService.write(board);
+
+        return "";
     }
 }
